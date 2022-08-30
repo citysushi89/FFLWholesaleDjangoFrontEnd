fc8fe45f847466374e41057037c0fad0f7686e1d
Owen <owenpickard89@gmail.com>
2022-08-30 09:58:30 -0400

    Gitgardian notes

--- PPsW_Django/settings.py
+++ PPsW_Django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# TODO hide in env before actual production
 SECRET_KEY = 'django-insecure-h)!$s==&zh_x-$vz*%7_$@eqwe!n*y=ra+bz_l3%9@(9)ntj+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
